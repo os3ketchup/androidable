93b4be9c7ba0138fddcecbf7f36a3eadaea42f6f
Eldor Teshaboev <eldorteshaboyev1994@gmail.com>
2023-07-02 21:44:34 +0500

    Initial commit

---
+++ app/src/main/java/uz/os3ketchup/androidable/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.os3ketchup.androidable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
